0900a9f207c8d7df3e737b4760dd908a3f74951c
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-05-31 12:17:12 +0300

    add demo to the project

---
+++ src/main/java/com/epam/loggerproject/Demo.java
@@ -0,0 +1,9 @@
+package com.epam.loggerproject;
+
+public class Demo {
+    public static void main(String[] args) {
+//        DataBase db = new DataBase();
+//
+//        db.addConnection();
+    }
+}
