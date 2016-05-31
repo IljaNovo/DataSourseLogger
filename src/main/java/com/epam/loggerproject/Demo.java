583df294665c28a3358f6520586d11f5590a3525
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-05-31 14:21:54 +0300

    delete file Demo

--- src/main/java/com/epam/loggerproject/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.epam.loggerproject;
-
-public class Demo {
-    public static void main(String[] args) {
-//        DataBase db = new DataBase();
-//
-//        db.addConnection();
-    }
-}
